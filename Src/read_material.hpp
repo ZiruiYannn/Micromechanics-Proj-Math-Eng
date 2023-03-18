12739f7bccafda0ae591ecacbe80f77f3880393d
AndreasDevogel <andreas.devogel@telenet.be>
2023-03-18 15:29:01 +0100

    Added header file for reading in materials from structured text file, together with basic test

---
+++ Src/read_material.hpp
@@ -0,0 +1,73 @@
+#ifndef read_material_hpp
+#define read_material_hpp
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include "Eigen/Dense"
+#include "unsupported/Eigen/CXX11/Tensor"
+
+// read in the Lamé coefficients of every material present in the cuboid and read in the material for every voxel in the cuboid 
+auto read_material(std::string const & filename) {
+    std::string line, lambda, mu, t;
+    int num_mat, xdim, ydim, zdim;
+    
+    std::ifstream file(filename);
+
+    // read in number of materials present in the cuboid and the Lamé coefficients for every material
+    std::getline(file, line);
+    std::istringstream split_line(line);
+    split_line >> num_mat;
+
+    Eigen::MatrixXd c(num_mat, 2);
+
+    for (int i = 0; i < num_mat; i++) {
+        std::getline(file, line);
+        split_line = std::istringstream(line);
+        split_line >> lambda;
+        split_line >> mu;
+        c(i, 0) = std::stod(lambda);
+        c(i, 1) = std::stod(mu);
+    }
+
+    // read in the dimensions of the cuboid and the material index for every voxel
+    std::getline(file, line);
+    split_line = std::istringstream(line);
+    split_line >> xdim;
+    split_line >> ydim;
+    split_line >> zdim;
+
+    Eigen::Tensor<int, 3> mat(xdim, ydim, zdim);
+
+    for (int k = 0; k < zdim; k++) {
+        for (int j = 0; j < zdim; j++) {
+            for (int i = 0; i < zdim; i++) {
+                std::getline(file, line);
+                mat(i, j, k) = std::stoi(line);
+            }
+        }
+    }
+
+    // read in the period for every dimension of the cuboid
+    Eigen::VectorXd prds(3);
+
+    std::getline(file, line);
+    split_line = std::istringstream(line);
+    for (int i = 0; i < 3; i++) {
+        split_line >> t;
+        prds(i) = std::stod(t); 
+    }
+
+    file.close();
+
+    // return all arguments in a custom struct
+    struct args {
+        Eigen::MatrixXd c;
+        Eigen::Tensor<int, 3> mat;
+        Eigen::VectorXd prds;
+    };
+
+    return args{c, mat, prds};
+}
+
+#endif
---
+++ Test/IO_test.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+#include "Eigen/Dense"
+#include "unsupported/Eigen/CXX11/Tensor"
+#include "read_material.hpp"
+
+TEST(InTest, BasicTest) {
+    double nu = 0.28;
+    double mod = 210e9;
+    double lambda = mod*nu / ((1+nu)*(1-2*nu));
+    double mu = mod / (2*(1+nu));
+
+    auto [c, mat, prds] = read_material("../input/test.in"); 
+    
+    // check if dimensions are as expected
+    ASSERT_EQ(c.rows(), 1);
+    ASSERT_EQ(c.cols(), 2);
+
+    ASSERT_EQ(mat.rank(), 3);
+    for (int i = 0; i < 3; i++){
+        ASSERT_EQ(mat.dimension(i), 2);
+    }
+
+    ASSERT_EQ(prds.size(), 3);
+
+
+    // check if contents are as expected
+    EXPECT_DOUBLE_EQ(c(0, 0), lambda);
+    EXPECT_DOUBLE_EQ(c(0, 1), mu);
+
+    EXPECT_EQ(mat(1, 1, 1), 0);
+
+    EXPECT_DOUBLE_EQ(prds(2), 2.0);
+}

80303f61635c590ad01539699d25018141d67ef5
AndreasDevogel <andreas.devogel@telenet.be>
2023-03-23 18:34:37 +0100

    Created test file and updated CMakeList to build algorithmTest

---
+++ Test/algorithmTest.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+#include "Eigen/Dense"
+#include "unsupported/Eigen/CXX11/Tensor"
+#include "fftw3.h"
+#include "micromechanics.hpp"
+
+class AlgorithmTest: public ::testing::Test {
+    protected:
+    void SetUp() override {
+        // create micromechanics instance
+    }
+
+    // micromechanics instance
+};
+
+TEST_F(AlgorithmTest, Initialization) {
+    // check if micromechanics instance is correctly initialized
+}

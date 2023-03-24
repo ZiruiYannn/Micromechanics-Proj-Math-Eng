2c5d7c0eb9eed412f9296e6f072040bcf145f884
AndreasDevogel <andreas.devogel@telenet.be>
2023-03-24 15:24:57 +0100

    Updated CMakeList and set up test suite for algorihm_test

--- Test/algorithmTest.cpp
+++ Test/algorithmTest.cpp
@@ -7,21 +7,21 @@
 class AlgorithmTest: public ::testing::Test {
     protected:
     void SetUp() override {
-        return; // do some things if necessary
+        Eigen::ArrayXd E(6);
+        E << 1.0, -0.5, -0.5, 0.0, 0.0, 0.0;
+        Eigen::Tensor<int, 3> mat(2, 3, 4);
+        mat.setConstant(0);
+        Eigen::ArrayXXd c(2, 2);
+        c << 1.0, 3.0, 2.0, 4.0;
+        Eigen::ArrayXd prds(3);
+        prds << 2.0, 3.0, 4.0;
+        double tol = 1e-4;
+        int maxit = 50;
+
+        m = new mme::micromechanics<>(E, mat, c, prds, tol, maxit);
     }
 
-    Eigen::ArrayXd E(6);
-    E << 1.0, -0.5, -0.5, 0.0, 0.0, 0.0;
-    Eigen::Tensor<int, 3> = mat(2, 3, 4);
-    mat.setConstant(0);
-    Eigen::ArrayXXd c(2, 2);
-    c << 1.0, 3.0, 2.0, 4.0;
-    Eigen::ArrayXd prds(3);
-    prds << 2.0, 3.0, 4.0;
-    double tol = 1e-4;
-    int maxit = 50;
-
-    micromechanics<double> m(E, mat, c, prds, tol, maxit);
+    mme::micromechanics<double> m;
 };
 
 TEST_F(AlgorithmTest, Initialization) {

f678e2a84ade94c602b761dded5adf7f02c74374
AndreasDevogel <andreas.devogel@telenet.be>
2023-03-24 14:44:16 +0100

    Created unit test overview and test fixture

--- Test/algorithmTest.cpp
+++ Test/algorithmTest.cpp
@@ -7,12 +7,45 @@
 class AlgorithmTest: public ::testing::Test {
     protected:
     void SetUp() override {
-        // create micromechanics instance
+        return; // do some things if necessary
     }
 
-    // micromechanics instance
+    Eigen::ArrayXd E(6);
+    E << 1.0, -0.5, -0.5, 0.0, 0.0, 0.0;
+    Eigen::Tensor<int, 3> = mat(2, 3, 4);
+    mat.setConstant(0);
+    Eigen::ArrayXXd c(2, 2);
+    c << 1.0, 3.0, 2.0, 4.0;
+    Eigen::ArrayXd prds(3);
+    prds << 2.0, 3.0, 4.0;
+    double tol = 1e-4;
+    int maxit = 50;
+
+    micromechanics<double> m(E, mat, c, prds, tol, maxit);
 };
 
 TEST_F(AlgorithmTest, Initialization) {
     // check if micromechanics instance is correctly initialized
 }
+
+TEST_F(AlgorithmTest, stressCompute) {
+    // check if function stressCompute works
+}
+
+TEST_F(AlgorithmTest, error) {
+    // check if function error works
+}
+
+TEST_F(AlgorithmTest, waveVec) {
+    // check if function waveVec works
+}
+
+TEST_F(AlgorithmTest, greenOp) {
+    // check if function greenOp works
+}
+
+TEST_F(AlgorithmTest, polarization) {
+    // check if function polarization works
+}
+
+

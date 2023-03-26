c204d3da9f4398c601381414c4e0ab5043dfb2be
AndreasDevogel <andreas.devogel@telenet.be>
2023-03-26 12:28:38 +0200

    debugged read_material function

--- Src/visualization.py
+++ Src/visualization.py
@@ -8,14 +8,19 @@ def read_material(filename):
     
     incr = [prds[idx] / (dims[idx] - 1) for idx in range(3)]
     
-    x, y, z, values = []
-    for k in range(dims(2)):
-        for j in range(dims(1)):
-            for i in range(dims(0)):
+    x = []
+    y = []
+    z = []
+    values = []
+    for k in range(dims[2]):
+        for j in range(dims[1]):
+            for i in range(dims[0]):
                 x.append(incr[0]*i)
                 y.append(incr[1]*j)
                 z.append(incr[2]*k)
                 values.append(float(file.readline()))
+            
+    file.close()
     
     return x, y, z, values
 
